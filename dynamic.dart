9da98ab3cf161215ec31ec3fcd5f9dc3f8d33de4
Ahmad Diffa <147916548+Diffajl@users.noreply.github.com>
2024-10-20 08:08:59 +0700

    Create dynamic.dart

---
+++ dynamic.dart
@@ -0,0 +1,14 @@
+// dynamic
+
+main() {
+  dynamic variable;
+  
+  variable = 10;
+  print(variable);
+  variable = 'string';
+  print(variable);
+  variable = true;
+  print(variable);
+  variable = 3.14;
+  print(variable);
+}
