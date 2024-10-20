6ad458257beeaf0e42f9b4ac2c91fb748aaeff39
Ahmad Diffa <147916548+Diffajl@users.noreply.github.com>
2024-10-20 08:48:40 +0700

    Create convertion.dart

---
+++ convertion.dart
@@ -0,0 +1,20 @@
+main() {
+
+  var inputString = '1000';
+  var inputInt = int.parse(inputString);
+  var inputDouble = double.parse(inputString);
+
+  print(inputString);
+  print(inputInt);
+  print(inputDouble);
+
+  var intToDouble = inputInt.toDouble();
+  var doubleToInt = inputDouble.toInt();
+
+  var intToString = inputInt.toString();
+  var doubleToString = inputDouble.toString();
+  
+  print(inputString);
+  print(inputInt);
+  print(inputDouble);
+}
