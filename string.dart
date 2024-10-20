2dc42f9691327b5c61059860ef1290654b9f18cb
Ahmad Diffa <147916548+Diffajl@users.noreply.github.com>
2024-10-20 08:14:30 +0700

    Create string.dart

---
+++ string.dart
@@ -0,0 +1,21 @@
+// string
+
+main() {
+  String firstName = 'Bambang';
+  String lastName = 'Kurnia';
+  
+  print(firstName);
+  print(lastName);
+  
+  String fullName = '${firstName} ${lastName}';
+  print(fullName);
+  
+  String text = '\'dart\' is cool';
+  print(text);
+  
+  String longString = '''
+  this is long string
+  ''';
+  
+  print(longString);
+}
